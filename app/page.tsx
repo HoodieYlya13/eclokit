1a37b1bffb4fa50c95249eb66e604cbc95ef845a
Ylya <ylyamartchenko@gmail.com>
2026-04-13 21:47:39 +0200

    small home page UI fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { HashScrollFix } from "@/components/hash-scroll-fix";
 import { BestSellers } from "@/components/sections/best-sellers";
 import { BoxOfTheMonth } from "@/components/sections/box-of-the-month";
 import { Hero } from "@/components/sections/hero";
-import { NewArrivals } from "@/components/sections/new-arrivals";
+// import { NewArrivals } from "@/components/sections/new-arrivals";
 import { OurPartners } from "@/components/sections/our-partners";
 import { OurValues } from "@/components/sections/our-values";
-import { Philosophy } from "@/components/sections/philosophy";
+// import { Philosophy } from "@/components/sections/philosophy";
 import { Subscriptions } from "@/components/sections/subscriptions";
 
 export default function Home() {
@@ -18,8 +18,8 @@ export default function Home() {
 			<Subscriptions limit={3} />
 			<OurValues />
 			<BestSellers limit={4} />
-			<NewArrivals limit={4} />
-			<Philosophy />
+			{/* <NewArrivals limit={4} />
+			<Philosophy /> */}
 		</>
 	);
 }
